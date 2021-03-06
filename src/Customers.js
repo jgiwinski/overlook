4c797eecc264062d1d3177568f6f460a80618ed6
jgiwinski <juliaiwinski@gmail.com>
2021-03-05 19:00:54 -0700

    Hook up API to DOM

--- src/Customers.js
+++ src/Customers.js
@@ -1,8 +1,19 @@
 class Customer {
-  constructor(customer) {
+  constructor(customer, rooms, bookings) {
     this.id = customer.id;
     this.name = customer.name;
+    this.roomsBooked = [];
   }
+
+  findRoomsBooked (bookings, rooms) {
+    let filter = bookings.filter(room => this.id === room.uersId);
+    
+  }
+
+  calculateTotalSpent () {
+
+  }
+
 }
 
 export default Customer;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import Customer from './Customers';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+function start(values) {
+  let allPeople = makeCustomers(values[0])
+}
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const customerData = fetchData('/customers', 'customers')
+const roomData = fetchData('/rooms', 'rooms')
+const bookingData = fetchData('/bookings', 'bookings')
+
+function fetchData(path, key) {
+  return fetch(`http://localhost:3001/api/v1${path}`)
+    .then(response => response.json())
+    .then(data => data[key])
+    .catch(error => console.log(error))
+}
+
+function makeCustomers(data) {
+  return data.map(customer => new Customer(customer))
+}
+
+Promise.all([customerData, roomData, bookingData])
+  .then(values => start(values))
